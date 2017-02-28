8eea2afd3bb52075c2ded45c3c8e41f63edc380b
Fedde Schaeffer <fedde@thehyve.nl>
2017-02-28 19:20:18 +0100

    List commands to run with alternative configuration

---
+++ run_with_alternative_properties.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+java -jar target/hobby-0.0.1-SNAPSHOT.jar --spring.profiles.active=test
---
+++ run_with_external_properties.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+java -jar target/hobby-0.0.1-SNAPSHOT.jar --spring.config.location=file:./application-external.properties
