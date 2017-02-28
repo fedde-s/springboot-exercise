9f6e1841d23a2b4ec3c4b4f7738aac8c8a67a406
Fedde Schaeffer <fedde@thehyve.nl>
2017-02-28 19:13:42 +0100

    Add stub queries to the repository

--- src/main/java/nl/thehyve/fedde/springcourse/persistence/CellRepository.java
+++ src/main/java/nl/thehyve/fedde/springcourse/persistence/CellRepository.java
@@ -1,6 +1,8 @@
 package nl.thehyve.fedde.springcourse.persistence;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import nl.thehyve.fedde.springcourse.model.Cell;
@@ -8,4 +10,12 @@ import nl.thehyve.fedde.springcourse.model.Cell;
 
 @Repository
 public interface CellRepository extends CrudRepository<Cell, Long> {
+
+    @Modifying
+    @Query("DELETE FROM Cell")
+    public Integer clear();
+
+    // TODO: query by a simple attribute, test if it needs to be findByName
+    //public Cell bluh(String name);
+
 }
